7ade49544d797d7624439e73a46af6e5be886e2c
Richard Parnaby-King <r.parnaby-king@suttonsilver.co.uk>
2016-05-24 13:12:34 +0100

    When click on notification, show the corresponding tab

--- bg.js
+++ bg.js
@@ -2,6 +2,7 @@
  * @copyright: Richard Parnaby-King
  * @url: http://richard.parnaby-king.co.uk
  */
+var notifications = {};
 chrome.extension.onMessage.addListener(function(msg,sender,sendResponse) {
 	chrome.notifications.create(
 		'', //create new notification id
@@ -10,8 +11,15 @@ chrome.extension.onMessage.addListener(function(msg,sender,sendResponse) {
 			iconUrl: 'meta-fail.png',
 			title: 'Meta Robots Checker',
 			message: msg,
-			priority: 0
+            contextMessage: sender.tab.title,
+			priority: 0,
+            isClickable: true
 		},
-		function() {}
-	);	
+		function(notificationId) {
+            notifications[notificationId] = sender.tab.id;
+        }
+	);
+    chrome.notifications.onClicked.addListener(function(notificationId){
+        chrome.tabs.update(notifications[notificationId], {active: true});
+    });
 });
